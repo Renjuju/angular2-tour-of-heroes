db2a5ef3a9c3e6a8b4e3b4206d08e1dde33ced6a
Renju Radhakrishnan <renju467@gmail.com>
2016-09-04 18:37:31 -0400

    adding http

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
 import { FormsModule }   from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
+import { HttpModule }    from '@angular/http';
+
+import { AppComponent }   from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent }     from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService }         from './hero.service';
 import { routing } from './app.routing';
-import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, routing ],
+  imports:      [ BrowserModule, FormsModule, routing, HttpModule ],
   declarations: [AppComponent, DashboardComponent, HeroDetailComponent, HeroesComponent],
   bootstrap: [AppComponent],
   providers: [HeroService]
